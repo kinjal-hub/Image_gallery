c1bd26b448de665246ab7712052977c77e9a1b2a
Kinjal <christian.kinju@gmail.com>
2025-12-10 11:17:15 +0530

    change in maincontext.js

--- src/mainContext.js
+++ src/mainContext.js
@@ -1,64 +1,25 @@
 import { createContext, useState, useContext } from 'react';
 
 export const MainContext = createContext({
-  favoriteCards: [],
-  addFavoriteCard: (card) => {},
-  incrementQuantity: (cardId) => {},
-  decrementQuantity: (cardId) => {},
+  favoriteCards: [], 
+  addFavoriteCard: (card) => {}, 
 });
 
 export const useMainContext = () => useContext(MainContext);
 
 export const MainContextProvider = ({ children }) => {
-  const [favoriteCards, setFavoriteCards] = useState([]);
   
-  const addFavoriteCard = (newCard) => {
-    setFavoriteCards(prevCards => {
-      const existingCardIndex = prevCards.findIndex(card => card.id === newCard.id);
+  
+  const [favoriteCards, setFavoriteCards] = useState([]); 
 
-      if (existingCardIndex > -1) {
-        
-        const updatedCards = [...prevCards];
-        updatedCards[existingCardIndex] = {
-          ...updatedCards[existingCardIndex],
-          quantity: updatedCards[existingCardIndex].quantity + 1
-        };
-        return updatedCards;
-      } else {
-        
-        return [...prevCards, { ...newCard, quantity: 1 }];
-      }
-    });
-  };
-  const incrementQuantity = (cardId) => {
-    setFavoriteCards(prevCards =>
-      prevCards.map(card =>
-        card.id === cardId
-          ? { ...card, quantity: card.quantity + 1 }
-          : card
-      )
-    );
+  
+  const addFavoriteCard = (card) => {
+    setFavoriteCards(prevCards => [...prevCards, card]); 
   };
 
-  const decrementQuantity = (cardId) => {
-    setFavoriteCards(prevCards =>
-      prevCards.map(card =>
-        card.id === cardId && card.quantity > 1
-          ? { ...card, quantity: card.quantity - 1 }
-          : card
-      )
-    );
-    
-  };
+  
   return (
-    <MainContext.Provider
-      value={{
-        favoriteCards,
-        addFavoriteCard,
-        incrementQuantity,
-        decrementQuantity,
-      }}
-    >
+    <MainContext.Provider value={{ favoriteCards, addFavoriteCard }}>
       {children}
     </MainContext.Provider>
   );
